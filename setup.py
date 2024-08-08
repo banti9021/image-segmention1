d136c1595fd48275e102a49c38304bce0438427a
banti9021 <nbanti943@gmail.com>
2024-08-08 09:41:22 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'banti9021',
+    author_email= 'nbanti943@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
